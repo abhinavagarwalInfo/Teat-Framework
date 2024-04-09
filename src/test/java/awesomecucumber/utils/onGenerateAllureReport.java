bdc14755c4328cb2c89edef8a44777cde8a56428
abhinavagarwalInfo <151527082+abhinavagarwalInfo@users.noreply.github.com>
2024-04-09 11:15:15 +0530

    Update onGenerateAllureReport.java
    Update Allure Reporting code to maintain historical data

--- src/test/java/awesomecucumber/utils/onGenerateAllureReport.java
+++ src/test/java/awesomecucumber/utils/onGenerateAllureReport.java
@@ -13,4 +13,26 @@ public class onGenerateAllureReport {
                 e.printStackTrace();
             }
         }
+
+        public void ExecuteCommand(){
+            try {
+                String pathCommand = "cd /Users/abhinavagarwal/workspace/Web-Teat-Framework/"; // Example command: list files in current directory
+                Process process = Runtime.getRuntime().exec(pathCommand);
+                int exitCode = process.waitFor();
+                System.out.println("Exited with error code: " + exitCode);
+
+                String AllureReportCommand = "allure generate allure-results --clean -o allure-report"; // Create Allure Report in allure-report folder
+                Process process1 = Runtime.getRuntime().exec(AllureReportCommand);
+                int exitCode1 = process1.waitFor();
+                System.out.println("Exited with error code: " + exitCode1);
+
+                String AllureResultCommand = "allure serve allure-results"; // Create report
+                Process process2 = Runtime.getRuntime().exec(AllureResultCommand);
+                int exitCode2 = process2.waitFor();
+                System.out.println("Exited with error code: " + exitCode2);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
 }
